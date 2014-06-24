8a343bea7711901fde70ac625a1dace28bc525f8
hsz <jakub@chrzanowski.info>
2014-06-24 13:42:12 +0200

    syntax highlight fix

--- src/mobi/hsz/idea/gitignore/GitignoreFileTypeFactory.java
+++ src/mobi/hsz/idea/gitignore/GitignoreFileTypeFactory.java
@@ -1,5 +1,6 @@
 package mobi.hsz.idea.gitignore;
 
+import com.intellij.openapi.fileTypes.ExactFileNameMatcher;
 import com.intellij.openapi.fileTypes.FileTypeConsumer;
 import com.intellij.openapi.fileTypes.FileTypeFactory;
 import mobi.hsz.idea.gitignore.lang.GitignoreLanguage;
@@ -8,6 +9,7 @@ import org.jetbrains.annotations.NotNull;
 public class GitignoreFileTypeFactory extends FileTypeFactory {
     @Override
     public void createFileTypes(@NotNull FileTypeConsumer consumer) {
+        consumer.consume(GitignoreFileType.INSTANCE, new ExactFileNameMatcher(GitignoreLanguage.FILENAME));
         consumer.consume(GitignoreFileType.INSTANCE, GitignoreLanguage.EXTENSION);
     }
 }
