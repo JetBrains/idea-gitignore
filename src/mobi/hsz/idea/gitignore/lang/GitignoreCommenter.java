13f1dbe222c3eebe0389be3b30c734f827bcbb38
hsz <jakub@chrzanowski.info>
2014-07-08 23:49:53 +0200

    Commenter support

---
+++ src/mobi/hsz/idea/gitignore/lang/GitignoreCommenter.java
@@ -0,0 +1,36 @@
+package mobi.hsz.idea.gitignore.lang;
+
+import com.intellij.lang.Commenter;
+import org.jetbrains.annotations.Nullable;
+
+public class GitignoreCommenter implements Commenter {
+    @Nullable
+    @Override
+    public String getLineCommentPrefix() {
+        return "#";
+    }
+
+    @Nullable
+    @Override
+    public String getBlockCommentPrefix() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getBlockCommentSuffix() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getCommentedBlockCommentPrefix() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getCommentedBlockCommentSuffix() {
+        return null;
+    }
+}
