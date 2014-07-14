1c29b592c821a115a497b828b597844aee1fd0d6
hsz <jakub@chrzanowski.info>
2014-07-14 23:54:47 +0200

    cover entry, glob parser

--- src/mobi/hsz/idea/gitignore/codeInspection/GitignoreCoverEntryInspection.java
+++ src/mobi/hsz/idea/gitignore/codeInspection/GitignoreCoverEntryInspection.java
@@ -11,15 +11,15 @@ import com.intellij.util.Processor;
 import mobi.hsz.idea.gitignore.GitignoreBundle;
 import mobi.hsz.idea.gitignore.psi.GitignoreEntry;
 import mobi.hsz.idea.gitignore.psi.GitignoreFile;
+import mobi.hsz.idea.gitignore.util.Glob;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class GitignoreDuplicateEntryInspection extends LocalInspectionTool {
+public class GitignoreCoverEntryInspection extends LocalInspectionTool {
     @NotNull
     @Override
     public HighlightDisplayLevel getDefaultLevel() {
@@ -30,34 +30,46 @@ public class GitignoreDuplicateEntryInspection extends LocalInspectionTool {
     @Override
     public ProblemDescriptor[] checkFile(@NotNull PsiFile file, @NotNull InspectionManager manager, boolean isOnTheFly) {
         ProblemsHolder problemsHolder = new ProblemsHolder(manager, file, isOnTheFly);
-        final List<GitignoreEntry> entries = new ArrayList<GitignoreEntry>();
+        final Map<GitignoreEntry, GitignoreEntry> entries = new HashMap<GitignoreEntry, GitignoreEntry>();
 
         if (file instanceof GitignoreFile) {
             new Processor<PsiFile>() {
                 @Override
                 public boolean process(PsiFile file) {
-                    Map<String, GitignoreEntry> map = new HashMap<String, GitignoreEntry>();
+                    Map<GitignoreEntry, List<String>> map = new HashMap<GitignoreEntry, List<String>>();
                     for (PsiElement child = file.getFirstChild(); child != null; child = child.getNextSibling()) {
                         if (!(child instanceof GitignoreEntry)) {
                             continue;
                         }
+
                         GitignoreEntry entry = (GitignoreEntry) child;
                         String value = entry.getEntry().getText();
 
-                        if (map.containsKey(value)) {
-                            entries.add(entry);
-                        } else {
-                            map.put(value, entry);
-                        }
-                    }
+                        List<String> matched = Glob.findAsPaths(file.getVirtualFile().getParent(), value, false);
 
+                        for (GitignoreEntry recent : map.keySet()) {
+                            List<String> recentValues = map.get(recent);
+                            if (recentValues.size() == 0 || matched.size() == 0) {
+                                continue;
+                            }
+                            if (recentValues.containsAll(matched)) {
+                                entries.put(recent, entry);
+                            } else if (matched.containsAll(recentValues)) {
+                                entries.put(entry, recent);
+                            }
+                        }
+
+                        map.put(entry, matched);
+                    }
                     return true;
                 }
             }.process(file);
         }
 
-        for (GitignoreEntry entry : entries) {
-            problemsHolder.registerProblem(entry, GitignoreBundle.message("codeInspection.duplicateEntry.message", entry.getEntry().getText()));
+        for (Map.Entry set: entries.entrySet()) {
+            GitignoreEntry key = (GitignoreEntry) set.getKey();
+            GitignoreEntry value = (GitignoreEntry) set.getValue();
+            problemsHolder.registerProblem(key, GitignoreBundle.message("codeInspection.coverEntry.message", key.getEntry().getText(), value.getEntry().getText()));
         }
 
         return problemsHolder.getResultsArray();
--- src/mobi/hsz/idea/gitignore/util/Glob.java
+++ src/mobi/hsz/idea/gitignore/util/Glob.java
@@ -1,30 +1,163 @@
 package mobi.hsz.idea.gitignore.util;
 
-import com.esotericsoftware.wildcard.Paths;
 import com.intellij.openapi.vfs.VirtualFile;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
 
 public class Glob {
-    private static final String EXCLUDE = "!.git/";
+    public static final String EXCLUDE = "!.git/";
 
-    public static List<File> find(VirtualFile root, String glob) {
-        if (!glob.startsWith("/")) {
-            glob = "**/" + glob;
+    public static List<VirtualFile> find(VirtualFile root, String glob) {
+        return find(root, glob, true);
+    }
+
+    public static List<VirtualFile> find(VirtualFile root, String glob, boolean ignoreNested) {
+        List<File> files = new ArrayList<File>();
+        String regex = createRegex(glob);
+        return walk(root, root, regex, ignoreNested);
+    }
+
+    public static List<String> findAsPaths(VirtualFile root, String glob) {
+        return findAsPaths(root, glob, true);
+    }
+
+    public static List<String> findAsPaths(VirtualFile root, String glob, boolean ignoreNested) {
+        List<String> list = new ArrayList<String>();
+        List<VirtualFile> files = find(root, glob, ignoreNested);
+        for (VirtualFile file : files) {
+            list.add(Utils.getRelativePath(root, file));
         }
-        Paths paths = new Paths(root.getPath(), EXCLUDE, glob);
-        List<File> files = paths.getFiles();
+        return list;
+    }
 
-        ListIterator<File> it = files.listIterator();
-        while (it.hasNext()) {
-            if (!it.next().exists()) {
-                it.remove();
+    private static List<VirtualFile> walk(VirtualFile root, VirtualFile directory, String regex, boolean ignoreNested) {
+        List<VirtualFile> files = new ArrayList<VirtualFile>();
+
+        for (VirtualFile file : directory.getChildren()) {
+            String path = Utils.getRelativePath(root, file);
+            if (path.equals("/.git")) {
+                continue;
+            }
+            if (path.matches(regex)) {
+                files.add(file);
+            }
+            if (!ignoreNested && file.isDirectory()) {
+                files.addAll(walk(root, file, regex, false));
             }
         }
 
         return files;
+    }
+
+    public static String createRegex(String glob) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("^");
+        if (!glob.startsWith("/")) {
+            if (!glob.startsWith("*")) {
+                sb.append(".*");
+            }
+            sb.append("(/[^/]*)*");
+        }
+
+        boolean escape = false, star = false, bracket = false;
+        for (char ch : glob.toCharArray()) {
+            if (bracket && ch != ']') {
+                sb.append(ch);
+                continue;
+            }
+
+            if (ch == '*') {
+                if (escape) {
+                    sb.append("\\*");
+                    escape = false;
+                    star = false;
+                } else if (star) {
+                    sb.append("(/[^/]*)*?");
+                    star = false;
+                } else {
+                    star = true;
+                }
+                continue;
+            } else if (star) {
+                sb.append("[^/]*?");
+                star = false;
+            }
+
+            switch (ch) {
+
+                case '\\':
+                    if (escape) {
+                        sb.append("\\\\");
+                        escape = false;
+                    } else {
+                        escape = true;
+                    }
+                    break;
+
+                case '?':
+                    if (escape) {
+                        sb.append("\\?");
+                        escape = false;
+                    } else {
+                        sb.append('.');
+                    }
+                    break;
+
+                case '[':
+                    if (escape) {
+                        sb.append('\\');
+                        escape = false;
+                    } else {
+                        bracket = true;
+                    }
+                    sb.append(ch);
+                    break;
+
+                case ']':
+                    if (!bracket || escape) {
+                        sb.append('\\');
+                    }
+                    sb.append(ch);
+                    bracket = false;
+                    escape = false;
+                    break;
+
+                case '.':
+                case '(':
+                case ')':
+                case '+':
+                case '|':
+                case '^':
+                case '$':
+                case '@':
+                case '%':
+                    sb.append('\\');
+                    sb.append(ch);
+                    escape = false;
+                    break;
+
+                default:
+                    escape = false;
+                    sb.append(ch);
+
+            }
+        }
+
+        if (star) {
+            sb.append(".*");
+        }
+        if (glob.endsWith("/")) {
+            sb.append("?");
+        }
+        if (escape) {
+            // should not be alone
+        }
+        sb.append('$');
+
+        return sb.toString();
     }
 
 }
