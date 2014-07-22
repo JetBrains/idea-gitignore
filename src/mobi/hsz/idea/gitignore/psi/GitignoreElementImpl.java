1d63636c35ab0b0281c7e2cd02237a093db90d4c
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2014-07-23 01:26:39 +0400

    Introduce base element for gitIgnore psi

---
+++ src/mobi/hsz/idea/gitignore/psi/GitignoreElementImpl.java
@@ -0,0 +1,19 @@
+package mobi.hsz.idea.gitignore.psi;
+
+import com.intellij.extapi.psi.ASTWrapperPsiElement;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.impl.source.resolve.reference.ReferenceProvidersRegistry;
+import org.jetbrains.annotations.NotNull;
+
+public class GitignoreElementImpl extends ASTWrapperPsiElement {
+    public GitignoreElementImpl(ASTNode node) {
+        super(node);
+    }
+
+    @NotNull
+    @Override
+    public PsiReference[] getReferences() {
+        return ReferenceProvidersRegistry.getReferencesFromProviders(this);
+    }
+}
