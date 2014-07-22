7d7f9b85deccf90df19e75fd63ad9c905b9979c3
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2014-07-23 01:50:06 +0400

    Fix possible compatibility issues with 138.* builds

--- src/mobi/hsz/idea/gitignore/reference/GitReferenceSet.java
+++ src/mobi/hsz/idea/gitignore/reference/GitReferenceSet.java
@@ -6,14 +6,12 @@ import com.intellij.psi.*;
 import com.intellij.psi.impl.source.resolve.reference.impl.providers.FileReference;
 import com.intellij.psi.impl.source.resolve.reference.impl.providers.FileReferenceHelper;
 import com.intellij.psi.impl.source.resolve.reference.impl.providers.FileReferenceSet;
-import com.intellij.util.containers.ContainerUtil;
 import mobi.hsz.idea.gitignore.psi.GitignoreEntry;
 import mobi.hsz.idea.gitignore.util.Utils;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedList;
 
 public class GitReferenceSet extends FileReferenceSet {
     public GitReferenceSet(@NotNull GitignoreEntry element) {
@@ -43,28 +41,22 @@ public class GitReferenceSet extends FileReferenceSet {
             super(fileReferenceSet, range, index, text);
         }
 
-        @NotNull
         @Override
-        protected ResolveResult[] innerResolve(boolean caseSensitive) {
-            LinkedList<ResolveResult> result = ContainerUtil.newLinkedList();
+        protected void innerResolveInContext(@NotNull String text, @NotNull PsiFileSystemItem context, Collection<ResolveResult> result, boolean caseSensitive) {
+            super.innerResolveInContext(text, context, result, caseSensitive);
             PsiManager psiManager = PsiManager.getInstance(getElement().getProject());
-            for (PsiFileSystemItem context : getContexts()) {
-                VirtualFile contextVirtualFile = context.getVirtualFile();
-                if (contextVirtualFile != null) {
-                    String regexFromGlob = Utils.createRegexFromGlob(getCanonicalText());
-                    for (VirtualFile file : contextVirtualFile.getChildren()) {
-                        if (file.getName().matches(regexFromGlob)) {
-                            PsiFileSystemItem psiFileSystemItem = FileReferenceHelper.getPsiFileSystemItem(psiManager, file);
-                            if (psiFileSystemItem != null) {
-                                result.add(new PsiElementResolveResult(psiFileSystemItem));
-                            }
+            VirtualFile contextVirtualFile = context.getVirtualFile();
+            if (contextVirtualFile != null) {
+                String regexFromGlob = Utils.createRegexFromGlob(getCanonicalText());
+                for (VirtualFile file : contextVirtualFile.getChildren()) {
+                    if (file.getName().matches(regexFromGlob)) {
+                        PsiFileSystemItem psiFileSystemItem = FileReferenceHelper.getPsiFileSystemItem(psiManager, file);
+                        if (psiFileSystemItem != null) {
+                            result.add(new PsiElementResolveResult(psiFileSystemItem));
                         }
                     }
                 }
             }
-            return result.isEmpty()
-                    ? super.innerResolve(caseSensitive)
-                    : result.toArray(new ResolveResult[result.size()]);
         }
     }
 }
