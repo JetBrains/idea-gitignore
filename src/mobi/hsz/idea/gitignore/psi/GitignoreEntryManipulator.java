5c2f80a73ecfb496e0d46675dab9b48ec64912ee
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2014-07-23 01:36:47 +0400

    Implement rename refactoring

---
+++ src/mobi/hsz/idea/gitignore/psi/GitignoreEntryManipulator.java
@@ -0,0 +1,21 @@
+package mobi.hsz.idea.gitignore.psi;
+
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.AbstractElementManipulator;
+import com.intellij.psi.PsiFileFactory;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.util.IncorrectOperationException;
+import mobi.hsz.idea.gitignore.GitignoreLanguage;
+import mobi.hsz.idea.gitignore.file.GitignoreFileType;
+import org.jetbrains.annotations.NotNull;
+
+public class GitignoreEntryManipulator extends AbstractElementManipulator<GitignoreEntry> {
+    @Override
+    public GitignoreEntry handleContentChange(@NotNull GitignoreEntry entry, @NotNull TextRange textRange, String s) throws IncorrectOperationException {
+        GitignoreFile file = (GitignoreFile) PsiFileFactory.getInstance(entry.getProject())
+                .createFileFromText(GitignoreLanguage.FILENAME, GitignoreFileType.INSTANCE, textRange.replace(entry.getText(), s));
+        GitignoreEntry newEntry = PsiTreeUtil.findChildOfType(file, GitignoreEntry.class);
+        assert newEntry != null;
+        return (GitignoreEntry) entry.replace(newEntry);
+    }
+}
