26e667033cc116dc3a94b3fe7e885a2916a6d6b0
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2014-07-25 01:17:26 +0400

    Cleanup unused inspection
      - reimplement with visitor
      - reuse reference
      - add tests

---
+++ tests/mobi/hsz/idea/gitignore/inspections/GitignoreInspectionTestCase.java
@@ -0,0 +1,28 @@
+package mobi.hsz.idea.gitignore.inspections;
+
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
+import mobi.hsz.idea.gitignore.GitignoreLanguage;
+
+import java.io.File;
+
+abstract public class GitignoreInspectionTestCase extends LightPlatformCodeInsightFixtureTestCase {
+    @Override
+    protected String getTestDataPath() {
+        return new File("testData/inspections/" + name()).getAbsolutePath();
+    }
+
+    private String name() {
+        return StringUtil.decapitalize(StringUtil.trimEnd(StringUtil.trimStart(getClass().getSimpleName(), "Gitignore"), "InspectionTest"));
+    }
+
+    @Override
+    protected boolean isWriteActionRequired() {
+        return false;
+    }
+
+    protected void doHighlightingTest() {
+        myFixture.copyDirectoryToProject(getTestName(true), getTestName(true));
+        myFixture.testHighlighting(true, false, true, getTestName(true) + "/" + GitignoreLanguage.FILENAME);
+    }
+}
