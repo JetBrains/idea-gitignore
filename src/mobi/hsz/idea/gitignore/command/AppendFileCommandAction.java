dccae55921f75a2bd6e7f34aea027080779ae135
hsz <jakub@chrzanowski.info>
2014-07-25 01:49:39 +0200

    Fixed #17 Prevent adding duplicate entries

--- src/mobi/hsz/idea/gitignore/command/AppendFileCommandAction.java
+++ src/mobi/hsz/idea/gitignore/command/AppendFileCommandAction.java
@@ -1,11 +1,18 @@
 package mobi.hsz.idea.gitignore.command;
 
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
 import com.intellij.openapi.application.Result;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import mobi.hsz.idea.gitignore.GitignoreBundle;
+import mobi.hsz.idea.gitignore.GitignoreLanguage;
+import mobi.hsz.idea.gitignore.util.Utils;
 import org.jetbrains.annotations.NotNull;
 
 public class AppendFileCommandAction extends WriteCommandAction<PsiFile> {
@@ -27,6 +34,15 @@ public class AppendFileCommandAction extends WriteCommandAction<PsiFile> {
         }
         Document document = PsiDocumentManager.getInstance(project).getDocument(file);
         if (document != null) {
+            for (PsiElement element : file.getChildren()) {
+                if (content.equals(element.getText())) {
+                    Notifications.Bus.notify(new Notification(GitignoreLanguage.NAME,
+                            GitignoreBundle.message("action.appendFile.entryExists", content),
+                            "in " + Utils.getRelativePath(project.getBaseDir(), file.getVirtualFile()),
+                            NotificationType.WARNING), project);
+                    return;
+                }
+            }
             document.insertString(document.getTextLength(), "\n" + content.replace("\r", ""));
             PsiDocumentManager.getInstance(project).commitDocument(document);
         }
