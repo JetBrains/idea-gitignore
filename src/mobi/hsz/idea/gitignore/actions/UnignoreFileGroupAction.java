3c199899f88a959b91103b121d095b6252b91ef8
hsz <jakub@chrzanowski.info>
2018-09-17 18:43:18 +0200

    Not annotated parameter overrides @NotNull parameter

--- src/mobi/hsz/idea/gitignore/actions/UnignoreFileGroupAction.java
+++ src/mobi/hsz/idea/gitignore/actions/UnignoreFileGroupAction.java
@@ -73,7 +73,7 @@ public class UnignoreFileGroupAction extends IgnoreFileGroupAction {
      * @param e action event
      */
     @Override
-    public void update(AnActionEvent e) {
+    public void update(@NotNull AnActionEvent e) {
         boolean status = settings.isUnignoreActions();
         e.getPresentation().setVisible(status);
         if (status) {
