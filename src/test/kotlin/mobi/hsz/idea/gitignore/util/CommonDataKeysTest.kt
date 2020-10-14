350ec619aa888e86022b18adac1c9f7c0a113812
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2020-10-14 20:40:44 +0200

    CommonDataKeysTest: convert to Kotlin

---
+++ src/test/kotlin/mobi/hsz/idea/gitignore/util/CommonDataKeysTest.kt
@@ -0,0 +1,21 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package mobi.hsz.idea.gitignore.util
+
+import mobi.hsz.idea.gitignore.Common
+import org.junit.Test
+import java.lang.reflect.InvocationTargetException
+
+class CommonDataKeysTest : Common<CommonDataKeys>() {
+
+    @Test
+    @Throws(
+        InvocationTargetException::class,
+        NoSuchMethodException::class,
+        InstantiationException::class,
+        IllegalAccessException::class
+    )
+    fun testPrivateConstructor() {
+        privateConstructor(CommonDataKeys::class.java)
+    }
+}
