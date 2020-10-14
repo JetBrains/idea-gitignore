d46f4948a3f2b15c08ee453dfd2f598e0f6bc463
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2020-10-14 13:33:49 +0200

    CoverEntryInspectionTest, InspectionTestCase: convert to Kotlin,

---
+++ src/test/kotlin/mobi/hsz/idea/gitignore/inspections/CoverEntryInspectionTest.kt
@@ -0,0 +1,29 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package mobi.hsz.idea.gitignore.inspections
+
+import mobi.hsz.idea.gitignore.codeInspection.IgnoreCoverEntryInspection
+
+class CoverEntryInspectionTest : InspectionTestCase() {
+
+    @Throws(Exception::class)
+    public override fun setUp() {
+        super.setUp()
+        myFixture.enableInspections(IgnoreCoverEntryInspection::class.java)
+    }
+
+    @Throws(Exception::class)
+    fun testEmptyEntries() {
+        doHighlightingTest()
+    }
+
+    @Throws(Exception::class)
+    fun testDuplicates() {
+        doHighlightingTest()
+    }
+
+    @Throws(Exception::class)
+    fun testCovering() {
+        doHighlightingTest()
+    }
+}
