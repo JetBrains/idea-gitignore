072a9e4d6f0218014079659f25820befb4abb684
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2020-10-14 20:45:57 +0200

    MatcherUtilTest: convert to Kotlin

---
+++ src/test/kotlin/mobi/hsz/idea/gitignore/util/MatcherUtilTest.kt
@@ -0,0 +1,60 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package mobi.hsz.idea.gitignore.util
+
+import junit.framework.TestCase
+import mobi.hsz.idea.gitignore.Common
+import org.junit.Test
+import java.util.regex.Pattern
+
+class MatcherUtilTest : Common<MatcherUtil?>() {
+
+    @Test
+    fun testMatch() {
+        val pattern = Pattern.compile("foo")
+        val util = MatcherUtil()
+        TestCase.assertFalse(util.match(null, null))
+        TestCase.assertFalse(util.match(null, "foo"))
+        TestCase.assertFalse(util.match(pattern, null))
+        TestCase.assertFalse(util.match(pattern, "fo"))
+        TestCase.assertTrue(util.match(pattern, "foo"))
+        TestCase.assertTrue(util.match(pattern, "xfooy"))
+    }
+
+    @Test
+    fun testMatchAllParts() {
+        val partsA = arrayOf<String?>("foo")
+        val partsB = arrayOf<String?>("foo", "bar")
+        TestCase.assertFalse(MatcherUtil.matchAllParts(null, null))
+        TestCase.assertFalse(MatcherUtil.matchAllParts(null, "foo"))
+        TestCase.assertFalse(MatcherUtil.matchAllParts(partsA, null))
+        TestCase.assertFalse(MatcherUtil.matchAllParts(partsA, "fo"))
+        TestCase.assertTrue(MatcherUtil.matchAllParts(partsA, "foo"))
+        TestCase.assertTrue(MatcherUtil.matchAllParts(partsA, "xfooy"))
+        TestCase.assertFalse(MatcherUtil.matchAllParts(partsB, "xfooxba"))
+        TestCase.assertTrue(MatcherUtil.matchAllParts(partsB, "xfooxbar"))
+    }
+
+    @Test
+    fun testMatchAnyPart() {
+        val partsA = arrayOf<String?>("foo")
+        val partsB = arrayOf<String?>("foo", "bar")
+        TestCase.assertFalse(MatcherUtil.matchAnyPart(null, null))
+        TestCase.assertFalse(MatcherUtil.matchAnyPart(null, "foo"))
+        TestCase.assertFalse(MatcherUtil.matchAnyPart(partsA, null))
+        TestCase.assertFalse(MatcherUtil.matchAnyPart(partsA, "fo"))
+        TestCase.assertTrue(MatcherUtil.matchAnyPart(partsA, "foo"))
+        TestCase.assertTrue(MatcherUtil.matchAnyPart(partsA, "xfooy"))
+        TestCase.assertTrue(MatcherUtil.matchAnyPart(partsB, "xfooxba"))
+        TestCase.assertTrue(MatcherUtil.matchAnyPart(partsB, "xfooxbar"))
+    }
+
+    @Test
+    fun testGetParts() {
+        var pattern = Pattern.compile("foo[ba]rbuz.*hi")
+        TestCase.assertEquals(MatcherUtil.getParts(null).size, 0)
+        TestCase.assertEquals(MatcherUtil.getParts(pattern).size, 2)
+        pattern = Pattern.compile("$$!@[fd]")
+        TestCase.assertEquals(MatcherUtil.getParts(pattern).size, 0)
+    }
+}
