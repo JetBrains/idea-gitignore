05a15c524f355e03b505f04a3738cef064fc1018
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2020-11-09 09:05:36 +0100

    Debounced converted to Kotlin

---
+++ src/main/kotlin/mobi/hsz/idea/gitignore/util/Debounced.kt
@@ -0,0 +1,34 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package mobi.hsz.idea.gitignore.util
+
+import com.intellij.concurrency.JobScheduler
+import com.intellij.openapi.project.DumbAwareRunnable
+import java.util.concurrent.ScheduledFuture
+import java.util.concurrent.TimeUnit
+
+/**
+ * Debounced runnable class that allows to run command just once in case it was triggered to often.
+ */
+abstract class Debounced<T>(private val delay: Int) : DumbAwareRunnable {
+
+    /** Timer that depends on the given [.delay] value. */
+    private var timer: ScheduledFuture<*>? = null
+
+    /** Wrapper run() method to invoke [.timer] properly. */
+    override fun run() {
+        run(null)
+    }
+
+    /** Wrapper run() method to invoke [.timer] properly. */
+    fun run(argument: T?) {
+        timer?.cancel(false)
+        timer = JobScheduler.getScheduler().schedule(
+            DumbAwareRunnable { task(argument) },
+            delay.toLong(),
+            TimeUnit.MILLISECONDS
+        )
+    }
+
+    /** Task to run in debounce way. */
+    protected abstract fun task(argument: T?)
+}
