bc00ed9e071c99a2402746c3c9607d8dbaa6f544
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2020-11-09 09:44:41 +0100

    IgnoreEntryOccurrence converted to Kotlin

---
+++ src/main/kotlin/mobi/hsz/idea/gitignore/indexing/IgnoreEntryOccurrence.kt
@@ -0,0 +1,102 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package mobi.hsz.idea.gitignore.indexing
+
+import com.intellij.openapi.util.Pair
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.util.containers.ContainerUtil
+import org.apache.commons.lang.StringUtils
+import org.apache.commons.lang.builder.HashCodeBuilder
+import java.io.DataInput
+import java.io.DataOutput
+import java.io.IOException
+import java.io.Serializable
+
+/**
+ * Entry containing information about the [VirtualFile] instance of the ignore file mapped with the collection
+ * of ignore entries for better performance. Class is used for indexing.
+ */
+class IgnoreEntryOccurrence(private val url: String, items: List<Pair<String, Boolean>>) : Serializable {
+
+    val items = ContainerUtil.immutableList(items)
+
+    /**
+     * Returns current [VirtualFile].
+     *
+     * @return current file
+     */
+    var file: VirtualFile? = null
+        get() {
+            if (field == null && url.isNotEmpty()) {
+                field = VirtualFileManager.getInstance().findFileByUrl(url)
+            }
+            return field
+        }
+        private set
+
+    companion object {
+        /**
+         * Static helper to write given [IgnoreEntryOccurrence] to the output stream.
+         *
+         * @param out   output stream
+         * @param entry entry to write
+         * @throws IOException I/O exception
+         */
+        @JvmStatic
+        @Synchronized
+        @Throws(IOException::class)
+        fun serialize(out: DataOutput, entry: IgnoreEntryOccurrence) {
+            out.run {
+                writeUTF(entry.url)
+                writeInt(entry.items.size)
+                entry.items.forEach {
+                    writeUTF(it.first)
+                    writeBoolean(it.second)
+                }
+            }
+        }
+
+        /**
+         * Static helper to read [IgnoreEntryOccurrence] from the input stream.
+         *
+         * @param input input stream
+         * @return read [IgnoreEntryOccurrence]
+         */
+        @JvmStatic
+        @Synchronized
+        @Throws(IOException::class)
+        fun deserialize(input: DataInput): IgnoreEntryOccurrence {
+            val url = input.readUTF()
+            val items = mutableListOf<Pair<String, Boolean>>()
+
+            if (!StringUtils.isEmpty(url)) {
+                val size = input.readInt()
+                repeat((0 until size).count()) {
+                    items.add(Pair.create(input.readUTF(), input.readBoolean()))
+                }
+            }
+            return IgnoreEntryOccurrence(url, items)
+        }
+    }
+
+    /**
+     * Calculates hashCode with [.url] and [.items] hashCodes.
+     *
+     * @return entry hashCode
+     */
+    override fun hashCode() = HashCodeBuilder().append(url).apply {
+        items.forEach { append(it.first).append(it.second) }
+    }.toHashCode()
+
+    /**
+     * Checks if given object is equal to current [IgnoreEntryOccurrence] instance.
+     *
+     * @param other to check
+     * @return objects are equal.
+     */
+    override fun equals(other: Any?) = when {
+        other !is IgnoreEntryOccurrence -> false
+        url != other.url || items.size != other.items.size -> false
+        else -> items.indices.find { items[it].toString() != other.items[it].toString() } == null
+    }
+}
