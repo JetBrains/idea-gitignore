37c629e74b5b7f6ffc2c581f28e564f6d91d1b0b
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2020-11-09 10:11:25 +0100

    IgnoreElementImpl converted to Kotlin

---
+++ src/main/kotlin/mobi/hsz/idea/gitignore/psi/IgnoreElementImpl.kt
@@ -0,0 +1,20 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package mobi.hsz.idea.gitignore.psi
+
+import com.intellij.extapi.psi.ASTWrapperPsiElement
+import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiReference
+import com.intellij.psi.impl.source.resolve.reference.ReferenceProvidersRegistry
+
+/**
+ * Definition of [ASTWrapperPsiElement].
+ */
+open class IgnoreElementImpl(node: ASTNode?) : ASTWrapperPsiElement(node!!) {
+
+    /**
+     * Gets [PsiReference] list for given element.
+     *
+     * @return [PsiReference] list
+     */
+    override fun getReferences(): Array<PsiReference> = ReferenceProvidersRegistry.getReferencesFromProviders(this)
+}
