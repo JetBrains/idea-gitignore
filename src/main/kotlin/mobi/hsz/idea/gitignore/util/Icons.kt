82ba13e09578483af2c75026cd2fcebe1d8058ed
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2020-11-19 16:55:43 +0100

    Deprecated method com.intellij.openapi.util.IconLoader.getIcon(String) invocation

--- src/main/kotlin/mobi/hsz/idea/gitignore/util/Icons.kt
+++ src/main/kotlin/mobi/hsz/idea/gitignore/util/Icons.kt
@@ -9,86 +9,86 @@ import com.intellij.openapi.util.IconLoader
 object Icons {
 
     /** General ignore icon. */
-    val IGNORE = IconLoader.getIcon("/icons/icon.png")
+    val IGNORE = IconLoader.getIcon("/icons/icon.png", Icons::class.java)
 
     /** Bazaar icon. */
-    val BAZAAR = IconLoader.getIcon("/icons/icon_bazaar.png")
+    val BAZAAR = IconLoader.getIcon("/icons/icon_bazaar.png", Icons::class.java)
 
     /** Chefignore icon. */
-    val CHEF = IconLoader.getIcon("/icons/icon_chef.png")
+    val CHEF = IconLoader.getIcon("/icons/icon_chef.png", Icons::class.java)
 
     /** Cloud Foundry icon. */
-    val CLOUD_FOUNDRY = IconLoader.getIcon("/icons/icon_cf.png")
+    val CLOUD_FOUNDRY = IconLoader.getIcon("/icons/icon_cf.png", Icons::class.java)
 
     /** Cvsignore icon. */
-    val CVS = IconLoader.getIcon("/icons/icon_cvs.png")
+    val CVS = IconLoader.getIcon("/icons/icon_cvs.png", Icons::class.java)
 
     /** Darcs icon. */
-    val DARCS = IconLoader.getIcon("/icons/icon_darcs.png")
+    val DARCS = IconLoader.getIcon("/icons/icon_darcs.png", Icons::class.java)
 
     /** Dockerignore icon. */
-    val DOCKER = IconLoader.getIcon("/icons/icon_docker.png")
+    val DOCKER = IconLoader.getIcon("/icons/icon_docker.png", Icons::class.java)
 
     /** ESLint icon. */
-    val ESLINT = IconLoader.getIcon("/icons/icon_eslint.png")
+    val ESLINT = IconLoader.getIcon("/icons/icon_eslint.png", Icons::class.java)
 
     /** ElasticBeanstalk icon. */
-    val ELASTIC_BEANSTALK = IconLoader.getIcon("/icons/icon_elasticbeanstalk.png")
+    val ELASTIC_BEANSTALK = IconLoader.getIcon("/icons/icon_elasticbeanstalk.png", Icons::class.java)
 
     /** Git icon. */
-    val GIT = IconLoader.getIcon("/icons/icon_git.png")
+    val GIT = IconLoader.getIcon("/icons/icon_git.png", Icons::class.java)
 
     /** Google Cloud icon. */
-    val GCLOUD = IconLoader.getIcon("/icons/icon_gcloud.png")
+    val GCLOUD = IconLoader.getIcon("/icons/icon_gcloud.png", Icons::class.java)
 
     /** Kubernetes Helm icon. */
-    val HELM = IconLoader.getIcon("/icons/icon_helm.png")
+    val HELM = IconLoader.getIcon("/icons/icon_helm.png", Icons::class.java)
 
     /** Floobits icon. */
-    val FLOOBITS = IconLoader.getIcon("/icons/icon_floobits.png")
+    val FLOOBITS = IconLoader.getIcon("/icons/icon_floobits.png", Icons::class.java)
 
     /** Fossil icon. */
-    val FOSSIL = IconLoader.getIcon("/icons/icon_fossil.png")
+    val FOSSIL = IconLoader.getIcon("/icons/icon_fossil.png", Icons::class.java)
 
     /** Mercurial icon. */
-    val MERCURIAL = IconLoader.getIcon("/icons/icon_mercurial.png")
+    val MERCURIAL = IconLoader.getIcon("/icons/icon_mercurial.png", Icons::class.java)
 
     /** Jetpack icon. */
-    val JETPACK = IconLoader.getIcon("/icons/icon_jetpack.png")
+    val JETPACK = IconLoader.getIcon("/icons/icon_jetpack.png", Icons::class.java)
 
     /** JSHint icon. */
-    val JSHINT = IconLoader.getIcon("/icons/icon_jshint.png")
+    val JSHINT = IconLoader.getIcon("/icons/icon_jshint.png", Icons::class.java)
 
     /** Monotone icon. */
-    val MONOTONE = IconLoader.getIcon("/icons/icon_monotone.png")
+    val MONOTONE = IconLoader.getIcon("/icons/icon_monotone.png", Icons::class.java)
 
     /** Nodemon icon. */
-    val NODEMON = IconLoader.getIcon("/icons/icon_nodemon.png")
+    val NODEMON = IconLoader.getIcon("/icons/icon_nodemon.png", Icons::class.java)
 
     /** Npmignore icon. */
-    val NPM = IconLoader.getIcon("/icons/icon_npm.png")
+    val NPM = IconLoader.getIcon("/icons/icon_npm.png", Icons::class.java)
 
     /** NuxtJS icon. */
-    val NUXTJS = IconLoader.getIcon("/icons/icon_nuxtjs.png")
+    val NUXTJS = IconLoader.getIcon("/icons/icon_nuxtjs.png", Icons::class.java)
 
     /** Perforce icon. */
-    val PERFORCE = IconLoader.getIcon("/icons/icon_perforce.png")
+    val PERFORCE = IconLoader.getIcon("/icons/icon_perforce.png", Icons::class.java)
 
     /** Prettier icon. */
-    val PRETTIER = IconLoader.getIcon("/icons/icon_prettier.png")
+    val PRETTIER = IconLoader.getIcon("/icons/icon_prettier.png", Icons::class.java)
 
     /** StyleLint icon. */
-    val STYLELINT = IconLoader.getIcon("/icons/icon_stylelint.png")
+    val STYLELINT = IconLoader.getIcon("/icons/icon_stylelint.png", Icons::class.java)
 
     /** Stylint icon. */
-    val STYLINT = IconLoader.getIcon("/icons/icon_stylint.png")
+    val STYLINT = IconLoader.getIcon("/icons/icon_stylint.png", Icons::class.java)
 
     /** Swagger Codegen icon. */
-    val SWAGGER_CODEGEN = IconLoader.getIcon("/icons/icon_swagger-codegen.png")
+    val SWAGGER_CODEGEN = IconLoader.getIcon("/icons/icon_swagger-codegen.png", Icons::class.java)
 
     /** TeamFoundation icon. */
-    val TF = IconLoader.getIcon("/icons/icon_tf.png")
+    val TF = IconLoader.getIcon("/icons/icon_tf.png", Icons::class.java)
 
     /** Up icon. */
-    val UP = IconLoader.getIcon("/icons/icon_up.png")
+    val UP = IconLoader.getIcon("/icons/icon_up.png", Icons::class.java)
 }
