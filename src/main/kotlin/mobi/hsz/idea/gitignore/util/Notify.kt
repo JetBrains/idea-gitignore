3d634614fdb7c0c98a0765f751c316ee5c8b1270
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2020-11-19 16:07:23 +0100

    Deprecated constructor NotificationGroup.<init>(String, NotificationDisplayType, boolean, String, Icon, int, DefaultConstructorMarker) invocation

--- src/main/kotlin/mobi/hsz/idea/gitignore/util/Notify.kt
+++ src/main/kotlin/mobi/hsz/idea/gitignore/util/Notify.kt
@@ -1,26 +1,18 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package mobi.hsz.idea.gitignore.util
 
-import com.intellij.notification.NotificationDisplayType
-import com.intellij.notification.NotificationGroup
+import com.intellij.notification.NotificationGroupManager
 import com.intellij.notification.NotificationListener
 import com.intellij.notification.NotificationType
 import com.intellij.notification.Notifications
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.wm.ToolWindowId
-import mobi.hsz.idea.gitignore.IgnoreBundle.message
 
 /**
  * Wrapper function for showing [Notification].
  */
 object Notify {
 
-    private val NOTIFICATION_GROUP = NotificationGroup(
-        message("notification.group"),
-        NotificationDisplayType.BALLOON,
-        true,
-        ToolWindowId.PROJECT_VIEW
-    )
+    private const val NOTIFICATION_GROUP_ID = ".ignore"
 
     /**
      * Shows [Notification].
@@ -32,7 +24,9 @@ object Notify {
      * @param listener optional listener
      */
     fun show(project: Project, title: String, content: String, type: NotificationType, listener: NotificationListener? = null) {
-        val notification = NOTIFICATION_GROUP.createNotification(title, content, type, listener)
+        val notification = NotificationGroupManager.getInstance()
+            .getNotificationGroup(NOTIFICATION_GROUP_ID)
+            .createNotification(title, content, type, listener)
         Notifications.Bus.notify(notification, project)
     }
 }
