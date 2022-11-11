dff14d26d8b0a796553a38f9abdd700228332fd6
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2022-11-11 22:39:45 +0100

    Fixes for deprecated methods

--- src/main/kotlin/mobi/hsz/idea/gitignore/foldableProjectView/StartupListener.kt
+++ src/main/kotlin/mobi/hsz/idea/gitignore/foldableProjectView/StartupListener.kt
@@ -4,16 +4,17 @@ import com.intellij.ide.AppLifecycleListener
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
+import com.intellij.openapi.startup.StartupActivity
 import mobi.hsz.idea.gitignore.settings.IgnoreSettings
 
-class StartupListener : AppLifecycleListener, ProjectManagerListener {
+class StartupListener : AppLifecycleListener, StartupActivity {
 
     private val settings = service<IgnoreSettings>()
     private val foldableProjectViewService = service<FoldableProjectViewService>()
 
     override fun welcomeScreenDisplayed() = advertise()
 
-    override fun projectOpened(project: Project) = advertise(project)
+    override fun runActivity(project: Project) = advertise(project)
 
     private fun advertise(project: Project? = null) {
         if (!settings.foldableProjectViewAdvertiseShown) {
