f90ce13ce2597a9496dabc2980f71f6f90f901b7
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2022-12-14 10:02:33 +0100

    dependencies update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,1 @@
 rootProject.name = "idea-gitignore"
-
-pluginManagement {
-    repositories {
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
-        gradlePluginPortal()
-    }
-}
